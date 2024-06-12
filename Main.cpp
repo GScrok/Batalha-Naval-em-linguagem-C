d86be0b01efbb67de6ac7101499f21da57f88ffd
GScrok <grmscrok@gmail.com>
2024-06-12 00:01:54 -0300

    Adicionado exibição do tabuleiro adversário no final com pontuação

--- Main.cpp
+++ Main.cpp
@@ -401,26 +401,35 @@ int main()
 		
 	    if (acertosJogador > acertosComputador)
 	    {
-	        printf("%s, voce afundou mais os navios do computador!\n", parametros->nome);
+	        printf("%s, voce afundou mais os navios do computador! %d - Navios", parametros->nome, acertosJogador);
 	        strcpy(parametros->vencedor, parametros->nome);
 	        parametros->pontuacao = acertosJogador;
-	        system("Winner.mp3");
+	        
+			system("Winner.mp3");
 	    }
 	    else if (acertosJogador < acertosComputador)
 	    {
-	        printf("O computador afundou mais os seus navios, %s!\n", parametros->nome);
+	        printf("O computador afundou mais os seus navios, %s! %d - Navios", parametros->nome, acertosComputador);
 	        strcpy(parametros->vencedor, "Computador");
 	        parametros->pontuacao = acertosComputador;
+			
+			printf("\nO tabuleiro do seu adversário era:\n");
+	        imprimirTabuleiro(tabuleiroComputador, 0);
+			
 			system("GameOver.mp3");
 	    }
 	    else {
-			printf("Voces tiveram um empate, %s!\n", parametros->nome);
+			printf("Voces tiveram um empate, %s! %d - Navios\n", parametros->nome, acertosJogador);
 			strcpy(parametros->vencedor, "EMPATE");
 			parametros->pontuacao = acertosJogador;
-	        system("GameOver.mp3");
+	        
+			printf("\nO tabuleiro do seu adversário era:\n");
+	        imprimirTabuleiro(tabuleiroComputador, 0);
+			
+			system("GameOver.mp3");
 	    }
 	    
-		printf("Pressione qualquer tecla para continuar!\n");
+		printf("\nPressione qualquer tecla para continuar!\n");
 	    getch();
 	    //insere no vetor de structs na posicao do contador o struct de parametros atual
 	    parametrosRegistros[contador] = parametros;
